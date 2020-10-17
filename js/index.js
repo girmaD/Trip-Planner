d2c8c69e09359f3e91d2b7d55ad47122747d957f
ziieng <cjengelhardt@gmail.com>
2020-10-16 22:50:31 -0700

    Code in place for creating new plan object

--- js/index.js
+++ js/index.js
@@ -1,16 +1,126 @@
 $('.ui.modal').modal();
+let acct = ""
+let tkn = ""
+gen()
 
-// $('.ui.dropdown').dropdown()
-
-$('.ui.dropdown').dropdown()
-$('.ui.dropdown').dropdown('toggle')
-
-$('.ui.modal').modal('show');
+//check for existing plan - if none found show the new plan modal
+if (localStorage.getItem("tripPlanStorage") == null) {
+    $('.ui.dropdown').dropdown()
+    $('.ui.modal').modal('show');
+} else {
+    console.log("nope")
+    //if plan exists, parse it and call function to write plan cards
+    let daysPlan = JSON.parse(localStorage.getItem("tripPlanStorage"))
+    writePlan(daysPlan)
+}
 
 
 $("#submit-button").on("click", function () {
     event.preventDefault();
-    console.log($("#arrival-date").val())
-    console.log($("#departure-date").val())
-    console.log($("#cityPick").dropdown('get value'))
+    let arrive = dayjs($("#arrival-date").val(), "YYYY-MM-DD")
+    let depart = dayjs($("#departure-date").val(), "YYYY-MM-DD")
+    let city = $("#cityPick").dropdown('get value')
+    if (arrive != null && depart != null && city != null) {
+        if (dayjs(arrive).isAfter(depart)) {
+            $("#departure-date").after('<p style="color:red">Departure date must be AFTER arrival date.</p>')
+            return false
+        }
+        $('.ui.modal').modal('hide');
+        createPlan(arrive, depart, city)
+    }
 })
+//set up a new trip plan
+function createPlan(arrive, depart, city) {
+    $.ajax({
+        url: "https://www.triposo.com/api/20200803/location.json?id=" + city + "&account=" + acct + "&token=" + tkn,
+        method: "GET"
+    }).done(function (response) {
+        let daysPlan = {
+            'city': {
+                'id': city,
+                'name': response.results[0].name,
+                'coords': {
+                    'lat': response.results[0].coordinates.latitude,
+                    'lon': response.results[0].coordinates.longitude
+                }
+            },
+            'dayArr': [],
+        }
+        let x = dayjs(depart).diff(arrive, 'day')
+        for (i = 0; i <= x; i++) {
+            let nDay = dayjs(arrive).add(i, 'day')
+            let newDay = {
+                'date': dayjs(nDay).format('YYYYMMDD'),
+                'act': {},
+                'rest': {},
+                'notes': ""
+            }
+            daysPlan.dayArr.push(newDay)
+        }
+        console.log(daysPlan)
+        localStorage.setItem(JSON.stringify("tripPlanStorage"))
+        writePlan(daysPlan)
+    });
+}
+
+function writePlan(daysPlan) {
+
+}
+
+function gen() {
+    //make stuff harder to copy paste
+    let maker = "B"
+    maker = maker + "0"
+    maker = maker + "H"
+    maker = maker + "P"
+    maker = maker + "O"
+
+    acct = acct + maker[0]
+    acct = acct + maker[3]
+    acct = acct + maker[2]
+    acct = acct + maker[1]
+    acct = acct + maker[0]
+    acct = acct + maker[4]
+    acct = acct + maker[2]
+    acct = acct + maker[2]
+
+    let ptA = "uh5"
+    let ptB = "agyl"
+    let ptC = "izr2"
+    let ptD = "p98"
+    maker = "fvxt"
+    maker = maker + ptA + ptB + ptC + ptD
+
+    tkn = tkn + maker[4]
+    tkn = tkn + maker[5]
+    tkn = tkn + maker[2]
+    tkn = tkn + maker[15]
+    tkn = tkn + maker[11]
+    tkn = tkn + maker[0]
+    tkn = tkn + maker[1]
+    tkn = tkn + maker[6]
+    tkn = tkn + maker[4]
+    tkn = tkn + maker[12]
+    tkn = tkn + maker[12]
+    tkn = tkn + maker[13]
+    tkn = tkn + maker[14]
+    tkn = tkn + maker[7]
+    tkn = tkn + maker[8]
+    tkn = tkn + maker[11]
+    tkn = tkn + maker[5]
+    tkn = tkn + maker[9]
+    tkn = tkn + maker[10]
+    tkn = tkn + maker[2]
+    tkn = tkn + maker[16]
+    tkn = tkn + maker[9]
+    tkn = tkn + maker[1]
+    tkn = tkn + maker[2]
+    tkn = tkn + maker[2]
+    tkn = tkn + maker[7]
+    tkn = tkn + maker[17]
+    tkn = tkn + maker[9]
+    tkn = tkn + maker[14]
+    tkn = tkn + maker[9]
+    tkn = tkn + maker[13]
+    tkn = tkn + maker[3]
+}
